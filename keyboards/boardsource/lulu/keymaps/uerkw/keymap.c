9eb19aa63ea59e77eaca0b6d8219c67b4a199619
uerkw <38047633+uerkw@users.noreply.github.com>
2024-08-24 10:43:03 -0400

    Remove Function keys from raise layer; unnecesary

--- keyboards/boardsource/lulu/keymaps/uerkw/keymap.c
+++ keyboards/boardsource/lulu/keymaps/uerkw/keymap.c
@@ -77,9 +77,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT(
   RGB_MOD, RGB_RMOD,RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD,                     RGB_HUI, RGB_HUD, _______, _______, _______, _______,
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                       KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, XXXXXXX, 
-  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,   RGB_TOG, _______,  KC_HOME, KC_PGDN, KC_PGUP,  KC_END, XXXXXXX, _______,
-                             _______, _______, _______,  _______, _______,  _______, _______, _______
+  _______, _______, _______, _______, _______, _______,                     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, XXXXXXX, 
+  _______, _______, _______, _______, _______, _______, RGB_TOG, _______,  KC_HOME, KC_PGDN, KC_PGUP,  KC_END, XXXXXXX, _______,
+                             _______, _______, _______, _______, _______,  _______, _______, _______
 ),
 /* ADJUST
  * ,-----------------------------------------.                    ,-----------------------------------------.
